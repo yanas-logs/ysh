6fb3bf2cf32aa0ef553b1ab894c2b4a8acc714af
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2024-11-05 16:00:15 +0000

    Create respond function for greeting and exit commands; add user input loop

--- ysh.sh
+++ ysh.sh
@@ -14,4 +14,27 @@ function welcome() {
     echo "Welcome to ysh,you've just launched shell! $greeting"
 }
 
+function respond() {
+    local input="$1"
+    case $input in
+        "hi"|"hello")
+            echo "Hi! How can I assist you?"
+            ;;
+        "exit")
+            echo "Exiting ysh. See you next time!"
+            exit 0
+            ;;
+        *)
+            echo "Sorry, I do not understand the command: '$input'"
+            ;;
+    esac
+}
+
 welcome
+
+while true; do
+    read -p "user@pc : " user_input
+    respond "$user_input"
+done
+
+
